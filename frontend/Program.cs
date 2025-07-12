d3ccd18286928d7dbbdc2ce490e36605ac3c757c
Gados László Márk <gadosmark7@gmail.com>
2025-07-12 17:04:50 +0200

    importing blazor.bootstrap create the visual prototype

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,11 +1,22 @@
 using frontend;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Logging.SetMinimumLevel(LogLevel.Debug);//
+//builder.Logging.AddConsole();
+
+builder.Services.AddBlazorBootstrap();
+
+
+
 
 await builder.Build().RunAsync();
---
+++ frontend/Shared/FlyerModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace frontend.Shared
+{
+    public class FlyerModel
+    {
+        public string flyerID {  get; set; }
+
+        public string SupermarketID { get; set; }
+
+        [JsonPropertyName("pageIndex")]
+        public int PageIndex {  get; set; }
+
+        [JsonPropertyName("url")]
+        public string ImageURL { get; set; }
+
+        public string ActualDate { get; set; }
+    }
+}
