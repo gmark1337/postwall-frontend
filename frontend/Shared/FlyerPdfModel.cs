bcfba602d8180c08897a525335707f363e54111f
Gados László Márk <gadosmark7@gmail.com>
2025-07-30 15:37:10 +0200

    Create model for pdf type flyers and create Penny's part. Added lazy loading and image loading priority for performance

---
+++ frontend/Shared/FlyerPdfModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace frontend.Shared
+{
+    public class FlyerPdfModel
+    {
+        public string FlyerPdfId { get; set; }
+
+        public string SupermarketId {  get; set; }
+
+        public string ActualDate { get; set; }
+
+        public string FirstPageURL { get; set; }
+
+
+        [JsonPropertyName("URL")]
+        public string FlyerPdfURL { get; set; }
+    }
+}
