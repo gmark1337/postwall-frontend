4788dad25152c481505d593f1f1b0d2f6d1d0a7f
Gados László Márk <gadosmark7@gmail.com>
2025-08-05 11:14:36 +0200

    a solution for NullabilityInfoContext_NotSupported fix

--- frontend/Shared/FlyerModel.cs
+++ frontend/Shared/FlyerModel.cs
@@ -5,19 +5,19 @@ namespace frontend.Shared
     public class FlyerModel
     {
         [JsonPropertyName("flyerID")]
-        public string flyerID { get; set; }
+        public string flyerID { get; set; } = string.Empty;
 
         [JsonPropertyName("supermarketID")]
-        public string SupermarketID { get; set; }
+        public string SupermarketID { get; set; } = string.Empty;
 
         [JsonPropertyName("pageIndex")]
         public int PageIndex { get; set; }
 
         [JsonPropertyName("url")]
-        public string ImageURL { get; set; }
+        public string ImageURL { get; set; } = string.Empty;
 
         [JsonPropertyName("actualDate")]
 
-        public string ActualDate { get; set; }
+        public string ActualDate { get; set; } = string.Empty;
     }
 }
--- frontend/Shared/FlyerPdfModel.cs
+++ frontend/Shared/FlyerPdfModel.cs
@@ -5,20 +5,20 @@ namespace frontend.Shared
     public class FlyerPdfModel
     {
         [JsonPropertyName("pdfID")]
-        public string FlyerPdfID { get; set; }
+        public string FlyerPdfID { get; set; } = string.Empty;
 
         [JsonPropertyName("supermarketId")]
-        public string SupermarketID {  get; set; }
+        public string SupermarketID { get; set; } = string.Empty;
 
         [JsonPropertyName("ActualDate")]
-        public string ActualDate { get; set; }
+        public string ActualDate { get; set; } = string.Empty;
 
 
         [JsonPropertyName("FirstPageURL")]
-        public string FirstPageURL { get; set; }
+        public string FirstPageURL { get; set; } = string.Empty;
 
 
         [JsonPropertyName("URL")]
-        public string FlyerPdfURL { get; set; }
+        public string FlyerPdfURL { get; set; } = string.Empty;
     }
 }
